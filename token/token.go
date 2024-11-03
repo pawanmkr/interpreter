e0b9f30c6b6d15de7d402cad9f7c71263dd3b0b1
Pawan Kumar <iampawanmkr@gmail.com>
2024-11-03 10:48:29 +0530

    tokenizer

---
+++ token/token.go
@@ -0,0 +1,46 @@
+package token
+
+type TokenType string
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	// identifiers + literals
+	IDENT = "IDENT"
+	INT = "INT"
+
+	// operators
+	ASSIGN = "="
+	PLUS = "+"
+
+	// DELIMITERS
+	COMMA = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// KEYWORDS
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)
+
+var keywords = map[string]TokenType {
+	"fn": FUNCTION,
+	"let": LET,
+}
+
+// if a word is ident or keyword
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
